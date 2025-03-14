884c0ca234865e4b379262b8de3da7b02de831df
Shehram <m.shehram01@gmail.com>
2025-03-14 11:00:21 -0700

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p></p>
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
