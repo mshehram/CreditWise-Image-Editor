6a6a1e9b4257a9d23e062fc9f01a9fbe3cedec9b
Shehram <m.shehram01@gmail.com>
2025-07-12 11:31:47 -0900

    Initial commit

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
@@ -14,7 +14,7 @@ interface ImageItem {
 
 const Profile = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) redirect("/sign-in");
 
