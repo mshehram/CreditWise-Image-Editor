e817bb6d2ce009469849746a235d21b5e31b5092
Shehram <m.shehram01@gmail.com>
2025-07-12 10:51:21 -0900

    Initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,20 +16,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         <h1 className="home-heading">
           Unleash Your Creative Vision with   CreditWise Image Editor
         </h1>
-        <ul className="flex-center w-full gap-20">
-          {navLinks.slice(1, 5).map((link) => (
-            <Link
-              key={link.route ?? `default-${Math.random()}`} // Fix: Ensure key is always a valid string
-              href={link.route || "#"}
-              className="flex-center flex-col gap-2"
-            >
-              <li className="flex-center w-fit rounded-full bg-white p-4">
-                <Image src={link.icon} alt="image" width={24} height={24} />
-              </li>
-              <p className="p-14-medium text-center text-white">{link.label}</p>
-            </Link>
-          ))}
-        </ul>
+        
       </section>
 
       <section className="sm:mt-12">
